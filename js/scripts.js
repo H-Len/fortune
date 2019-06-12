48af41e933fdd75fb413624c7e419d845074b17c
Hannah Melendy and Mike Lofgren <hannah+mike@>
2019-06-11 17:52:04 -0700

    create functionality in js

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,24 @@ $(document).ready(function() {
 
   $("#fortune-survey").submit(function (event) {
     event.preventDefault();
-
     var fortuneTotal = 0;
 
     $("input:checkbox[name=occur]:checked").each(function() {
-      fortuneTotal += parseInt($(".happened").val());
-      console.log(fortuneTotal);
+      fortuneTotal += parseInt($(this).val());
     });
+    console.log(fortuneTotal);
+
+    var badReading = "I'm sorry... I'll leave it at that.";
+    var goodReading = "Wow! You're lucky!";
+    var aReading = "Neutral. Have a great day.";
+
+    if(fortuneTotal < 0) {
+      alert(badReading);
+    } else if(fortuneTotal > 0) {
+      alert(goodReading);
+    } else {
+      alert(aReading);
+    }
 
   });
 });
